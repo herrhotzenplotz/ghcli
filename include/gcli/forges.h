5005d3911c4d91548133b047fac1291928b76441
Nico Sonack <nsonack@herrhotzenplotz.de>
2024-05-21 13:30:55 +0200

    GL/#224: Indentation Cleanup
    Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>

--- include/gcli/forges.h
+++ include/gcli/forges.h
@@ -509,17 +509,17 @@ struct gcli_forge_descriptor {
 
 	/**
 	 * Status summary for the account */
-    int (*get_notifications)(
-	    struct gcli_ctx *ctx,
-	    int max,
+	int (*get_notifications)(
+		struct gcli_ctx *ctx,
+		int max,
 		struct gcli_notification_list *notifications);
 
 	/**
 	 * *Given an issue notification grab the associated issue. */
-    int (*notification_get_issue)(
-	    struct gcli_ctx *ctx,
-	    struct gcli_notification const *const notification,
-	    struct gcli_issue *out);
+	int (*notification_get_issue)(
+		struct gcli_ctx *ctx,
+		struct gcli_notification const *const notification,
+		struct gcli_issue *out);
 
 	/**
 	 * Given a notification fetch the target's comments */
