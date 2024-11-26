herrhotzenplotz/ghcli

commit 038c07a780a40f6d1f73e779bb724ad77521a190

cmdconfig: Avoid running into documentation bug by adding alias for api-base
We had a documentation bug and inconsistency regarding the api-base
config value in the configuration file of gcli.

Essentially the tool was searching for apibase where the documentation
said it was looking for api-base (without a dash).

Add an alias and only document api-base as a default. The latter
is to not break existing configurations.

Issue-tracker: https://github.com/herrhotzenplotz/gcli/issues/138
Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>

src/cmd/cmdconfig.c
@@ -748,8 +748,19 @@ gcli_config_get_apibase(struct gcli_ctx *ctx)
 	char *url = NULL;
 
 	if (acct) {
-		sn_sv url_sv = gcli_config_find_by_key(ctx, acct, "apibase");
-		if (url_sv.length)
+		sn_sv url_sv = {0};
+
+		url_sv = gcli_config_find_by_key(ctx, acct, "api-base");
+
+		/* https://github.com/herrhotzenplotz/gcli/issues/138
+		 *
+		 * Above is correct behaviour. Below is bad/buggy behaviour.
+		 * Check for the second (buggy) option apibase and
+		 * use it if needed. */
+		if (sn_sv_null(url_sv))
+			gcli_config_find_by_key(ctx, acct, "apibase");
+
+		if (!sn_sv_null(url_sv))
 			url = sn_sv_to_cstr(url_sv);
 	}
 

commit 8839f856b11f6562886c5a0cbad477b93caf3e0d

hotfix: add missing assignment of apibase url
I accidentially forgot this assignment in:
	038c07a (cmdconfig: Avoid running into documentation bug by adding alias for api-base, 2024-11-18)

This lead to gcli using the default url for the forge type even if
told to use the different account.

Fixes: 038c07a (cmdconfig: Avoid running into documentation bug by adding alias for api-base)
Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>
Signed-off-by: Gavin-John Noonan <mail@gjnoonan.co.uk>

src/cmd/cmdconfig.c
@@ -758,7 +758,7 @@ gcli_config_get_apibase(struct gcli_ctx *ctx)
 		 * Check for the second (buggy) option apibase and
 		 * use it if needed. */
 		if (sn_sv_null(url_sv))
-			gcli_config_find_by_key(ctx, acct, "apibase");
+			url_sv = gcli_config_find_by_key(ctx, acct, "apibase");
 
 		if (!sn_sv_null(url_sv))
 			url = sn_sv_to_cstr(url_sv);