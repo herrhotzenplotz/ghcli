7f629a8ffdcee4821e3f729521160748bbc63c30
Nico Sonack <nsonack@herrhotzenplotz.de>
2025-01-24 16:42:16 +0100

    Bump copyright years
    Signed-off-by: Nico Sonack <nsonack@herrhotzenplotz.de>

--- src/cmd/cmd.c
+++ src/cmd/cmd.c
@@ -62,7 +62,7 @@ copyright(void)
 {
 	fprintf(
 		stderr,
-		"Copyright 2021, 2022, 2023 Nico Sonack <nsonack@herrhotzenplotz.de>"
+		"Copyright 2021-2025 Nico Sonack <nsonack@herrhotzenplotz.de>"
 		" and contributors.\n");
 }
 
